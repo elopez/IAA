90656a7b01d70b5f913eb146cd9db6fd3840338e
Emilio López <elopez93@gmail.com>
2016-03-25 11:42:28 -0300

    First attempt at TP0

---
+++ TP0/names-files.h
@@ -0,0 +1,11 @@
+#ifndef __NAMES_FILES_H__
+#define __NAMES_FILES_H__
+
+#define NAMES_CONTENT_A_B_HEAD "Class.\n\n"
+
+#define NAMES_CONTENT_C \
+	"Class.\n\n" \
+	"x: continuous.\n" \
+	"y: continuous.\n"
+
+#endif
---
+++ TP0/tp0.c
@@ -0,0 +1,253 @@
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <getopt.h>
+
+#include "names-files.h"
+
+long double P(long double sigma, long double mu, long double x)
+{
+	long double tmp = 1 / (sigma * sqrtl(2.0 * M_PIl));
+	long double up = -powl(x-mu, 2) / (2 * powl(sigma, 2));
+	tmp *= expl(up);
+	return tmp;
+}
+
+long double gen_point_under_P(long double sigma, long double mu)
+{
+	for (;;) {
+		long double y = drand48();
+		long double x = drand48();
+		x *= 10 * sigma;
+		x -= 5 * sigma - mu;
+
+		if (P(sigma, mu, x) >= y) {
+			return x;
+		}
+	}
+}
+
+void ejercicio_a(FILE *data, FILE *names, char *param)
+{
+	int d, n;
+	long double c;
+
+	if (param)
+		sscanf(param, "%d%d%Lf", &d, &n, &c);
+	else
+		scanf("%d%d%Lf", &d, &n, &c);
+
+	/* .data generation */
+
+	long double sigma = c * sqrtl(d);
+
+	for (int i = 0; i < n/2; i++) {
+		for (int j = 0; j < d; j++) {
+			fprintf(data, "%Lf, ", gen_point_under_P(sigma, 1));
+		}
+		fputs("0\n", data);
+	}
+
+	for (int i = 0; i < n/2; i++) {
+		for (int j = 0; j < d; j++) {
+			fprintf(data, "%Lf, ", gen_point_under_P(sigma, -1));
+		}
+		fputs("1\n", data);
+	}
+
+	/* .names generation */
+
+	fputs(NAMES_CONTENT_A_B_HEAD, names);
+	for (int i = 0; i < n; i++)
+		fprintf(names, "c%d: continuous.\n", i);
+
+}
+
+void ejercicio_b(FILE *data, FILE *names, char *param)
+{
+	int d, n;
+	long double c;
+
+	if (param)
+		sscanf(param, "%d%d%Lf", &d, &n, &c);
+	else
+		scanf("%d%d%Lf", &d, &n, &c);
+
+	/* .data generation */
+
+	long double sigma = c;
+
+	for (int i = 0; i < n/2; i++) {
+		fprintf(data, "%Lf, ", gen_point_under_P(sigma, 1));
+		for (int j = 1; j < d; j++) {
+			fprintf(data, "%Lf, ", gen_point_under_P(sigma, 0));
+		}
+		fputs("0\n", data);
+	}
+
+	for (int i = 0; i < n/2; i++) {
+		fprintf(data, "%Lf, ", gen_point_under_P(sigma, -1));
+		for (int j = 1; j < d; j++) {
+			fprintf(data, "%Lf, ", gen_point_under_P(sigma, 0));
+		}
+		fputs("1\n", data);
+	}
+
+	/* .names generation */
+
+	fputs(NAMES_CONTENT_A_B_HEAD, names);
+	for (int i = 0; i < n; i++)
+		fprintf(names, "c%d: continuous.\n", i);
+}
+
+struct point {
+	long double x;
+	long double y;
+};
+
+struct point gen_point_on_spiral(int s)
+{
+	struct point p;
+	long double ro, theta, rocurvetop, rocurvebot;
+
+	for (;;) {
+		p.x = drand48() * 2.2 - 1.1;
+		p.y = drand48() * 2.2 - 1.1;
+		ro = sqrtl(powl(p.x, 2) + powl(p.y, 2));
+		if (ro > 1)
+			continue;
+		theta = atan2l(p.y, p.x);
+//		theta = theta < 0 ? theta + 2 * M_PIl : theta;
+
+		int type = 0;
+		rocurvebot = theta / (4 * M_PIl);
+		rocurvetop = rocurvebot + 1.0/4.0;
+		for (int i = 0; i < 3; i++) {
+			if (ro > rocurvebot && ro < rocurvetop)
+				type = 1;
+			rocurvebot += 1.0/2.0;
+			rocurvetop += 1.0/2.0;
+		}
+
+		if (type == s)
+			return p;
+	}
+}
+
+void ejercicio_c(FILE *data, FILE *names, char *param)
+{
+	int n;
+
+	if (param)
+		sscanf(param, "%d", &n);
+	else
+		scanf("%d", &n);
+
+	/* .data generation */
+
+	for (int i = 0; i < n/2; i++) {
+		struct point p = gen_point_on_spiral(0);
+		fprintf(data, "%Lf, %Lf, 0\n", p.x, p.y);
+	}
+
+	for (int i = 0; i < n/2; i++) {
+		struct point p = gen_point_on_spiral(1);
+		fprintf(data, "%Lf, %Lf, 1\n", p.x, p.y);
+	}
+
+	/* .names generation */
+
+	fputs(NAMES_CONTENT_C, names);
+}
+
+
+int main(int argc, char **argv)
+{
+	long seed = time(NULL);
+	char *ejercicio = NULL, *salida = NULL, *param = NULL;
+	static struct option long_options[] = {
+		{"ejercicio", required_argument, 0, 'e'},
+		{"salida",    required_argument, 0, 'o'},
+		{"param",     required_argument, 0, 'p'},
+		{"semilla",   required_argument, 0, 's'},
+		{ /* sentinel */ }
+	};
+
+
+	int c, option_index = 0;
+	while (1) {
+		c = getopt_long(argc, argv, "e:o:p:s:",
+				long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 0:
+		case '?':
+			break;
+		case 'e':
+			ejercicio = strdup(optarg);
+			break;
+		case 'o':
+			salida = strdup(optarg);
+			break;
+		case 'p':
+			param = strdup(optarg);
+			break;
+		case 's': {
+			sscanf(optarg, "%ld", &seed);
+			break;
+		}
+		default:
+			abort();
+		}
+	}
+
+	/* Seed PRNG with user supplied seed (or current time) */
+	srand48(seed);
+
+	if (!ejercicio || !salida) {
+		fprintf(stderr, "Por favor especifique ejercicio (-e) y prefijo de salida (-o)\n");
+		return 1;
+	}
+
+	FILE *data, *names;
+	char filename[BUFSIZ];
+	sprintf(filename, "%s.data", salida);
+	data = fopen(filename, "w");
+	sprintf(filename, "%s.names", salida);
+	names = fopen(filename, "w");
+	if (!data || !names) {
+		fprintf(stderr, "Error abriendo archivos\n");
+		return 1;
+	}
+
+	switch (*ejercicio) {
+	case 'a':
+		ejercicio_a(data, names, param);
+		break;
+	case 'b':
+		ejercicio_b(data, names, param);
+		break;
+	case 'c':
+		ejercicio_c(data, names, param);
+		break;
+	default:
+		fprintf(stderr, "Ejercicio no existente\n");
+	}
+
+	fclose(data);
+	fclose(names);
+
+	free(ejercicio);
+	free(salida);
+	free(param);
+
+	return 0;
+}
