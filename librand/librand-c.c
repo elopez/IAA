380682ea84c6ff974a17a126b329344f7ff637c4
Emilio López <elopez93@gmail.com>
2016-03-25 16:32:51 -0300

    Improve seeding of C PRNG

--- librand/librand-c.c
+++ librand/librand-c.c
@@ -1,12 +1,19 @@
 #define _XOPEN_SOURCE
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "librand-internal.h"
 
 void librand_c_seed(unsigned seed)
 {
-	srand48(seed ? seed : (unsigned)time(NULL));
+	long fullseed = seed;
+	if (!seed) {
+		fullseed = time(NULL);
+		fullseed |= clock() << 16;
+		fullseed ^= getpid() << 8;
+	}
+	srand48(fullseed);
 }
 
 long double librand_c_gen(void)
