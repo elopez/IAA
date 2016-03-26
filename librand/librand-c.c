771fe08142e8a781110eed6614808d807b372498
Emilio López <elopez93@gmail.com>
2016-03-26 19:43:23 -0300

    Scale y to be more efficient on C normal distributions

--- librand/librand-c.c
+++ librand/librand-c.c
@@ -25,16 +25,8 @@ long double librand_c_gen_uniform(void)
 
 
 struct normal_params {
-	long double mu, sigma;
+	long double mu, sigma, u;
 };
-
-void *librand_c_init_normal(long double mu_p, long double sigma_p)
-{
-	struct normal_params *p = malloc(sizeof(*p));
-	p->mu = mu_p;
-	p->sigma = sigma_p;
-	return p;
-}
 
 static long double P(long double mu, long double sigma, long double x)
 {
@@ -42,6 +34,15 @@ static long double P(long double mu, long double sigma, long double x)
 	long double up = -powl(x-mu, 2) / (2 * powl(sigma, 2));
 	tmp *= expl(up);
 	return tmp;
+}
+
+void *librand_c_init_normal(long double mu_p, long double sigma_p)
+{
+	struct normal_params *p = malloc(sizeof(*p));
+	p->mu = mu_p;
+	p->sigma = sigma_p;
+	p->u = P(mu_p, sigma_p, mu_p);
+	return p;
 }
 
 long double librand_c_gen_normal(void *n)
@@ -54,6 +55,7 @@ long double librand_c_gen_normal(void *n)
 		long double x = librand_c_gen_uniform();
 		x *= 12 * sigma;
 		x -= 6 * sigma - mu;
+		y *= p->u;
 
 		if (P(mu, sigma, x) >= y) {
 			return x;
