e2506b3ba7e812ee0c782c98c3103aa172c4bb45
Emilio López <elopez93@gmail.com>
2016-03-27 14:45:47 -0300

    Improve TP0 plots

--- TP0/test/plot-ab.r
+++ TP0/test/plot-ab.r
@@ -1,3 +1,6 @@
 data = read.csv(paste(name, "data", sep="."))
-title=paste("Ejercicio", ex, "con parámetros", params)
-plot(data[,1], data[,2], xlab="X", ylab="Y", main=title, col=data[,3]+2)
+title = paste("Ejercicio", ex, "con parámetros", params)
+plot(data[,1], data[,2], col=data[,3]+2, main=title,
+     xlab="X", ylab="Y", xlim=c(-5,5), ylim=c(-5,5))
+axis(side=1, at=-5:5)
+axis(side=2, at=-5:5)
--- TP0/test/plot-c.r
+++ TP0/test/plot-c.r
@@ -1,3 +1,4 @@
 data = read.csv(paste(name, "data", sep="."))
 title=paste("Ejercicio C con", params, "puntos")
-plot(data[,1], data[,2], xlab="X", ylab="Y", main=title, col=data[,3]+2)
+plot(data[,1], data[,2], col=data[,3]+2, main=title,
+     xlab="X", ylab="Y", xlim=c(-1,1), ylim=c(-1,1))
