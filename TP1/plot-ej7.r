4e94d68a22825c3c01c9b55d741ee14116e1335f
Emilio López <elopez93@gmail.com>
2016-04-30 14:52:29 -0300

    TP1: Fix exercise 7 plot to match requirements

--- TP1/plot-ej7.r
+++ TP1/plot-ej7.r
@@ -1,15 +1,21 @@
 library("ggplot2")
 x = read.csv(file, header=F)
-a = aggregate(x[x[,1]=='a',4], list(x[x[,1]=='a',2]), mean)
-b = aggregate(x[x[,1]=='b',4], list(x[x[,1]=='b',2]), mean)
-atest = aes(y = x, colour = "A - Test")
-btest = aes(y = x, colour = "B - Test")
+a = aggregate(x[x[,1]=='a',3:4], list(x[x[,1]=='a',2]), mean)
+b = aggregate(x[x[,1]=='b',3:4], list(x[x[,1]=='b',2]), mean)
+atrain = aes(y = V3, colour = "A - Train")
+atest = aes(y = V4, colour = "A - Test")
+btrain = aes(y = V3, colour = "B - Train")
+btest = aes(y = V4, colour = "B - Test")
 ggplot(a, aes(Group.1)) +
   # A
+  geom_point(atrain) +
   geom_point(atest) +
+  geom_line(atrain) +
   geom_line(atest) +
   # B
+  geom_point(data = b, btrain) +
   geom_point(data = b, btest) +
+  geom_line(data = b, btrain) +
   geom_line(data = b, btest) +
   labs(y = "Error porcentual promedio") +
   labs(x = "Valor de d") +
