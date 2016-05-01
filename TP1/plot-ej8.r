b5c982d8760cbfa2ffdcbb079b92967a1dfd3499
Emilio López <elopez93@gmail.com>
2016-05-01 00:29:16 -0300

    TP1: Improve labels on plot-ej8.r

--- TP1/plot-ej8.r
+++ TP1/plot-ej8.r
@@ -1,3 +1,4 @@
 data = read.csv("xor.data", header=F)
+colnames(data) <- c("x", "y", "noise1", "noise2", "noise3", "noise4", "class")
 title="Features de dataset XOR"
 plot(data[,-7], col=data[,7]+2, main=title)
